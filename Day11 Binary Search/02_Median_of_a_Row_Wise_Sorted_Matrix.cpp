8da52ea0e34c3e89107a7059c04cfb1b7dd15456
utkarsh-009 <utkarshpatil321@gmail.com>
2023-01-06 12:02:22 +0530

    Finding Median of a Row Wise Sorted Matrix

--- Day11 Binary Search/02_Median_of_a_Row_Wise_Sorted_Matrix.cpp
+++ Day11 Binary Search/02_Median_of_a_Row_Wise_Sorted_Matrix.cpp
@@ -15,15 +15,13 @@ Problem Statement: Given a row wise sorted matrix of size R*C, find the median o
 #include <bits/stdc++.h>
 using namespace std;
 
-// Brute Force: [ TC: O(R*Clog(R*C)), AS: O(R*C) ]
+// Brute Force: [ TC: O(R*C*log(R*C)), AS: O(R*C) ]
 class Solution1
 {
 public:
     int median(vector<vector<int>> &matrix, int R, int C)
     {
-        // code here
         vector<int> v;
-
         for (int i = 0; i < R; i++)
         {
             for (int j = 0; j < C; j++)
@@ -39,16 +37,80 @@ public:
     }
 };
 
-// Efficient Binary Search [ TC: O(R*Clog(R*C)), AS: O(1) ]
+// Efficient Binary Search [ TC: O(R*log(max-min)*log(C)), AS: O(1) ]
 // Intution: For a number to be median, there should be exactly (n/2) numbers which are less than this number
 class Solution2
 {
 public:
+    // To find number of elements less than given number
+    int upperBound(vector<int> row, int mid)
+    {
+        int l = 0, h = row.size() - 1;
+        while (l <= h)
+        {
+            int m = (l + h) / 2;
+            if (row[m] <= mid)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                h = m - 1;
+            }
+        }
 
-    
+        return l;
+    }
 
     int median(vector<vector<int>> &matrix, int R, int C)
     {
+        // finding high and low i.e. minimum and maximum element in matrix
+        int low = INT_MAX, high = INT_MIN;
+        for (int i = 0; i < R; i++)
+        {
+            low = min(matrix[i][0], low);
+            high = max(matrix[i][C - 1], high);
+        }
 
+        // For a number to be median, there should be exactly (n/2) numbers which are less than this number
+        int desired = (R * C + 1) / 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int nums_less_than_mid = 0;
+
+            // counting total number of elements that are less than mid
+            for (int i = 0; i < R; i++)
+            {
+                nums_less_than_mid += upperBound(matrix[i], mid);
+            }
+
+            // if elments less than mid are less than desired => low = mid + 1
+            if (nums_less_than_mid < desired)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return low;
     }
 };
+
+int main()
+{
+
+    int r = 3;
+    int c = 3;
+    vector<vector<int>> matrix = {{1, 3, 5},
+                                  {2, 6, 9},
+                                  {3, 6, 9}};
+
+    Solution1 obj1;
+    Solution2 obj2;
+    cout << obj2.median(matrix, r, c);
+}
