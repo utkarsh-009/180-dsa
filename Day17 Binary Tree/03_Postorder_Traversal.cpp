c8b72b0e5ea936d4eefd1c0355a86ed5e81df087
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-05 14:51:40 +0530

    Postorder Traversal

---
+++ Day17 Binary Tree/03_Postorder_Traversal.cpp
@@ -0,0 +1,38 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//   Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Recursive Solution
+class Solution
+{
+public:
+    void postOrder(TreeNode *root, vector<int> &ans)
+    {
+        if (root != NULL)
+        {
+            postOrder(root->left, ans);
+            postOrder(root->right, ans);
+            ans.push_back(root->val);
+        }
+    }
+
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        postOrder(root, ans);
+
+        return ans;
+    }
+};
