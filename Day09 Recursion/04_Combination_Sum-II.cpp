687cefb57cfc42df4e7d7999b078fd5cf183c151
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-15 17:05:30 +0530

    Combination Sum II

---
+++ Day09 Recursion/04_Combination_Sum-II.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// [TC: O(2^N), AS: O(2^N)]
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    void helper(vector<int> &candidates, vector<int> v, int target, int curr_sum, int idx)
+    {
+        if (curr_sum >= target)
+        {
+            if (curr_sum == target)
+            {
+                ans.push_back(v);
+            }
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); i++)
+        {
+            if (i > idx && candidates[i] == candidates[i - 1])
+            {
+                continue;
+            }
+            curr_sum += candidates[i];
+            v.push_back(candidates[i]);
+            helper(candidates, v, target, curr_sum, i + 1);
+            curr_sum -= candidates[i];
+            v.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        sort(candidates.begin(), candidates.end());
+        vector<int> v;
+        helper(candidates, v, target, 0, 0);
+
+        return ans;
+    }
+};
