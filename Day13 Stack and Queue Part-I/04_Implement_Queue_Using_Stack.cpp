676d3377518220c6363cd1a96995186061d5239b
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-19 20:34:23 +0530

    Implement Queue using Stacks

--- Day13 Stack and Queue Part-I/04_Implement_Queue_Using_Stack.cpp
+++ Day13 Stack and Queue Part-I/04_Implement_Queue_Using_Stack.cpp
@@ -6,7 +6,8 @@ The implemented queue should support all the functions of a normal queue (push, 
 #include <bits/stdc++.h>
 using namespace std;
 
-class MyQueue
+// M1: O(n) push and rest all operations O(1)
+class MyQueue1
 {
 public:
     stack<int> st1, st2;
@@ -46,5 +47,52 @@ public:
     bool empty()
     {
         return (st1.size() == 0);
+    }
+};
+
+// M2[Amortised O(1)]: top and pop operation compromised to O(n) for push operations to be in O(1)
+/*
+- pop and peek operations are performed on output stack after emptying the input stack
+- elements are push inside the input stack
+*/
+class MyQueue2
+{
+public:
+    stack<int> input, output;
+
+    // O(1)
+    void push(int x)
+    {
+        input.push(x);
+    }
+
+    // O(n)
+    int pop()
+    {
+        int temp = peek();
+        output.pop();
+
+        return temp;
+    }
+
+    // O(n)
+    int peek()
+    {
+        if (output.empty())
+        {
+            while (input.size())
+            {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+
+        return output.top();
+    }
+
+    // O(1)
+    bool empty()
+    {
+        return (input.empty() && output.empty());
     }
 };
